807cf55ca5bf18cd45464316aa58abeb8ae61818
Sethanant Pipatpakorn <sethanant.p@icloud.com>
2021-10-31 16:53:37 +0700

    add unfinish

--- bank-transactions/src/Main.java
+++ bank-transactions/src/Main.java
@@ -5,7 +5,10 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
 
@@ -23,6 +26,13 @@ public class Main {
 
     public static void main(String[] args) throws IOException, CsvValidationException {
         ArrayList<Transaction>  transactions = readFromCSV("5000-BT-Records.csv");
+        List<Transaction> txs = transactions.stream()
+                .filter((Transaction t) -> t.getBalance() == 0)
+                .collect(Collectors.groupingBy(Transaction::getDescription))
+                .entrySet()
+                .stream()
+                .collect(Collectors.toList())
+
     }
 
 }
