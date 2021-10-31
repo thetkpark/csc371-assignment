7948b8112d27727c7012269c038f861eb5592448
Sethanant Pipatpakorn <sethanant.p@icloud.com>
2021-10-31 14:51:40 +0700

    add parse csv to transactions

--- bank-transactions/src/Main.java
+++ bank-transactions/src/Main.java
@@ -1,6 +1,28 @@
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static ArrayList<Transaction> readFromCSV(String csvFileName) throws IOException, CsvValidationException {
+        ArrayList<Transaction> transactions = new ArrayList<Transaction>();
+        CSVReader reader = new CSVReader(new FileReader(csvFileName));
+        reader.skip(1);
+        String[] line;
+        while ((line = reader.readNext()) != null) {
+            Transaction tx = new Transaction(line);
+            transactions.add(tx);
+        }
+        return transactions;
     }
+
+    public static void main(String[] args) throws IOException, CsvValidationException {
+        ArrayList<Transaction>  transactions = readFromCSV("5000-BT-Records.csv");
+    }
+
 }
--- bank-transactions/src/Transaction.java
+++ bank-transactions/src/Transaction.java
@@ -1,3 +1,68 @@
-public class Transaction {
+import javax.swing.text.DateFormatter;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
+public class Transaction {
+    public LocalDate date;
+    public String description;
+    public float deposit;
+    public float withdrawl;
+    public float balance;
+
+    public Transaction(String[] token) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d-MMM-yyyy");
+        this.date = LocalDate.parse(token[0], formatter);
+        this.description = token[1];
+        this.deposit = Float.parseFloat(token[2].replace("\"", "").replace(",", ""));
+        this.withdrawl = Float.parseFloat(token[3].replace("\"", "").replace(",", ""));
+        this.balance = Float.parseFloat(token[4].replace("\"", "").replace(",", ""));
+    }
+
+    public Transaction(LocalDate date, String description, float deposit, float withdrawl, float balance) {
+        this.date = date;
+        this.description = description;
+        this.deposit = deposit;
+        this.withdrawl = withdrawl;
+        this.balance = balance;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public float getDeposit() {
+        return deposit;
+    }
+
+    public void setDeposit(float deposit) {
+        this.deposit = deposit;
+    }
+
+    public float getWithdrawl() {
+        return withdrawl;
+    }
+
+    public void setWithdrawl(float withdrawl) {
+        this.withdrawl = withdrawl;
+    }
+
+    public float getBalance() {
+        return balance;
+    }
+
+    public void setBalance(float balance) {
+        this.balance = balance;
+    }
 }
