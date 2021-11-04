04d9f6b9be5b900caa8a8fb626b6a634d903ca17
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2021-11-04 21:47:58 +0700

    done i guess

--- bank-transactions/src/Main.java
+++ bank-transactions/src/Main.java
@@ -43,11 +43,14 @@ public class Main {
         ArrayList<Transaction>  transactions = readFromCSV("5000-BT-Records.csv");
         long task1Serial = runTask(transactions, false, Main::Task1Serialize);
         long task1Parallel = runTask(transactions, false, Main::Task1Parallel);
-        long task2Serial = runTask(transactions, false, Main::Task2Serialize);
-        long task2Parallel = runTask(transactions, false, Main::Task2Parallel);
+        long task21Serial = runTask(transactions, false, Main::Task21Serialize);
+        long task21Parallel = runTask(transactions, false, Main::Task21Parallel);
+        long task22Serial = runTask(transactions, false, Main::Task22Serialize);
+        long task22Parallel = runTask(transactions, false, Main::Task22Parallel);
 
         printSpeedupAndEfficiency(task1Serial, task1Parallel, "Task 1");
-        printSpeedupAndEfficiency(task2Serial, task2Parallel, "Task 2");
+        printSpeedupAndEfficiency(task21Serial, task21Parallel, "Task 2.1");
+        printSpeedupAndEfficiency(task22Serial, task22Parallel, "Task 2.2");
 
 //        System.out.println("");
 //        transactions = readFromCSV("5000000-BT-Records.csv");
@@ -100,12 +103,48 @@ public class Main {
                 });
     }
 
-    public static void Task2Serialize(ArrayList<Transaction> transactions, boolean print) {
+//    public static void Task21Serialize(ArrayList<Transaction> transactions, boolean print) {
+//        transactions.stream()
+//                .collect(Collectors.groupingBy(Transaction::getMonthYear))
+//                .entrySet()
+//                .stream()
+//                .forEach((key, value) -> {
+//
+//                });
+//    }
+
+    public static void Task21Serialize(ArrayList<Transaction> transactions, boolean print) {
+        transactions.stream()
+                .collect(Collectors.groupingBy(Transaction::getMonthYear))
+                .forEach((key, value) -> {
+                    DepositWithdrawls sum = new DepositWithdrawls();
+                    float depositSum = value.stream().map(Transaction::getDeposit).reduce(0f, Float::sum);
+                    float withdrawlsSum = value.stream().map(Transaction::getWithdrawl).reduce(0f, Float::sum);
+                    if (print) {
+                        System.out.println(key + ": Deposit " + depositSum + ", Withdrawls: " + withdrawlsSum);
+                    }
+                });
+    }
+
+    public static void Task21Parallel(ArrayList<Transaction> transactions, boolean print) {
+        transactions.parallelStream()
+                .collect(Collectors.groupingBy(Transaction::getMonthYear))
+                .forEach((key, value) -> {
+                    DepositWithdrawls sum = new DepositWithdrawls();
+                    float depositSum = value.parallelStream().map(Transaction::getDeposit).reduce(0f, Float::sum);
+                    float withdrawlsSum = value.parallelStream().map(Transaction::getWithdrawl).reduce(0f, Float::sum);
+                    if (print) {
+                        System.out.println(key + ": Deposit " + depositSum + ", Withdrawls: " + withdrawlsSum);
+                    }
+                });
+    }
+
+    public static void Task22Serialize(ArrayList<Transaction> transactions, boolean print) {
         transactions.stream()
                 .collect(Collectors.groupingBy(Transaction::getMonthYear))
                 .forEach((key, value) -> {
                     Transaction firstTx = value.get(0);
-                    float sum = firstTx.getBalance() + firstTx.getWithdrawl() - firstTx.getDeposit();
+                    float sum = firstTx.getBalance() + firstTx.getWithdrawl() - firstTx.getDeposit(); // Get initial balance
                     sum += value.stream()
                             .map(Transaction::sumDepositWithdrawl)
                             .reduce(0f, Float::sum);
@@ -113,7 +152,7 @@ public class Main {
                 });
     }
 
-    public static void Task2Parallel(ArrayList<Transaction> transactions, boolean print) {
+    public static void Task22Parallel(ArrayList<Transaction> transactions, boolean print) {
         transactions.parallelStream()
                 .collect(Collectors.groupingBy(Transaction::getMonthYear))
                 .forEach((key, value) -> {
@@ -126,4 +165,14 @@ public class Main {
                 });
     }
 
+}
+
+class DepositWithdrawls {
+     float deposit;
+     float withdrawls;
+
+    public DepositWithdrawls() {
+        this.deposit = 0;
+        this.withdrawls = 0;
+    }
 }
