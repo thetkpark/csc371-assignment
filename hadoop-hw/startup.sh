66f751a3bebc52d151866e1cbfbcbf3c9aef57da
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2021-11-14 21:45:54 +0700

    remove pdsh

--- hadoop-hw/startup.sh
+++ hadoop-hw/startup.sh
@@ -1,6 +1,4 @@
 #! /bin/bash
 sudo apt-get update && sudo apt-get -y dist-upgrade
 sudo apt-get -y install openjdk-8-jdk-headless
-sudo apt-get -y install pdsh
-mkdir hadoop-install && cd hadoop-install
-wget https://dlcdn.apache.org/hadoop/common/hadoop-3.3.1/hadoop-3.3.1-aarch64.tar.gz && tar xvzf hadoop-3.3.1-aarch64.tar.gz
+# wget https://dlcdn.apache.org/hadoop/common/hadoop-3.3.1/hadoop-3.3.1-aarch64.tar.gz && tar xvzf hadoop-3.3.1-aarch64.tar.gz
