78497c55a3eb998c6d477d7be6d288234914cc82
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2021-11-18 22:43:59 +0700

    use Text

--- hadoop-hw/MapReduce/src/Main.java
+++ hadoop-hw/MapReduce/src/Main.java
@@ -5,7 +5,7 @@ import java.util.UUID;
 import com.opencsv.CSVReader;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
@@ -15,21 +15,19 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 public class Main {
 
     public static class TokenizerMapper
-            extends Mapper<Object, String, String, Transaction>{
+            extends Mapper<Object, Text, Text, Transaction>{
 
-        private final static IntWritable one = new IntWritable(1);
-        private UUID uuid = UUID.randomUUID();
+        private String uuid = UUID.randomUUID().toString();
         private boolean pair = true;
 
-        public void map(Object key, String value, Context context
-        ) {
-            CSVReader reader = new CSVReader(new StringReader(value));
+        public void map(Object key, Text value, Context context) {
+            CSVReader reader = new CSVReader(new StringReader(value.toString()));
             try {
                 Transaction tx = new Transaction(reader.readNext());
-                context.write(uuid.toString(), tx);
+                context.write(new Text(uuid), tx);
                 if (!pair) {
-                    uuid = UUID.randomUUID();
-                    context.write(uuid.toString(), tx);
+                    uuid = UUID.randomUUID().toString();
+                    context.write(new Text(uuid), tx);
                 }
                 pair = !pair;
             } catch (Exception e) {
@@ -38,8 +36,8 @@ public class Main {
         }
     }
 
-    public static class SumSamePairReducer extends Reducer<String, Transaction, String, String> {
-        public void reduce(String key, Iterable<Transaction> txs, Context context) throws IOException, InterruptedException {
+    public static class SumSamePairReducer extends Reducer<Text, Transaction, Text, Text> {
+        public void reduce(Text key, Iterable<Transaction> txs, Context context) throws IOException, InterruptedException {
             Transaction[] txsList = new Transaction[2];
             int i = 0;
             for (Transaction tx : txs) {
@@ -48,7 +46,7 @@ public class Main {
 
             float balance = txsList[0].getBalance() + txsList[1].sumDepositWithdrawl();
             if (Math.abs(balance - txsList[1].getBalance()) > 0.001) {
-                context.write(key, txsList[1].toString());
+                context.write(key, new Text(txsList[1].toString()));
             }
         }
     }
@@ -76,8 +74,8 @@ public class Main {
         job.setMapperClass(TokenizerMapper.class);
         job.setCombinerClass(SumSamePairReducer.class);
         job.setReducerClass(SumSamePairReducer.class);
-        job.setOutputKeyClass(String.class);
-        job.setOutputValueClass(String.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
         System.exit(job.waitForCompletion(true) ? 0 : 1);
