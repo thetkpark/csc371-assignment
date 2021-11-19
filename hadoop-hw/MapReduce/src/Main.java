761ecf22c4fbeb175e86fa2c2122cffa7d58f2a9
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2021-11-19 14:55:51 +0700

    local float

--- hadoop-hw/MapReduce/src/Main.java
+++ hadoop-hw/MapReduce/src/Main.java
@@ -1,6 +1,4 @@
 import java.io.IOException;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.UUID;
 
 import org.apache.hadoop.conf.Configuration;
@@ -35,21 +33,23 @@ public class Main {
 
     public static class SumSamePairReducer extends Reducer<Text, Text, Text, Text> {
         public void reduce(Text key, Iterable<Text> textTxs, Context context) throws IOException, InterruptedException {
-            Transaction[] txs = new Transaction[2];
+
+            float[] balances = new float[2];
+            float[] deposit = new float[2];
+            float[] withdrawl = new float[2];
             int i = 0;
 
-//            String output = "";
             for (Text text : textTxs) {
                 String[] token = text.toString().split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)");
                 if (token.length < 5) {
-//                    context.write(key, text);
                     return;
                 } else {
-                    txs[i] = new Transaction(token);
-                    context.write(key, new Text(txs[i].toString()));
+                    deposit[i] = Float.parseFloat(token[2].replace("\"", "").replace(",", ""));
+                    withdrawl[i] = Float.parseFloat(token[3].replace("\"", "").replace(",", ""));
+                    balances[i] = Float.parseFloat(token[4].replace("\"", "").replace(",", ""));
                     i++;
+                    context.write(key, text);
                 }
-//                context.write(key, new Text(token.length + ""));
             }
 //            Transaction[] txsList = new Transaction[2];
 //            int i = 0;
@@ -65,90 +65,22 @@ public class Main {
     }
 
 
-    public static class Transaction {
-        public LocalDate date;
-        public String description;
-        public float deposit;
-        public float withdrawl;
-        public float balance;
+//    public static class Transaction {
+//        public LocalDate date;
+//        public String description;
+//        public float deposit;
+//        public float withdrawl;
+//        public float balance;
+//
+//        public Transaction(String[] token) {
+//            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d-MMM-yyyy");
+//            this.date = LocalDate.parse(token[0], formatter);
+//            this.description = token[1];
+//            this.deposit = Float.parseFloat(token[2].replace("\"", "").replace(",", ""));
+//            this.withdrawl = Float.parseFloat(token[3].replace("\"", "").replace(",", ""));
+//            this.balance = Float.parseFloat(token[4].replace("\"", "").replace(",", ""));
+//        }
 
-        public Transaction(String[] token) {
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d-MMM-yyyy");
-            this.date = LocalDate.parse(token[0], formatter);
-            this.description = token[1];
-            this.deposit = Float.parseFloat(token[2].replace("\"", "").replace(",", ""));
-            this.withdrawl = Float.parseFloat(token[3].replace("\"", "").replace(",", ""));
-            this.balance = Float.parseFloat(token[4].replace("\"", "").replace(",", ""));
-        }
-
-        public Transaction(LocalDate date, String description, float deposit, float withdrawl, float balance) {
-            this.date = date;
-            this.description = description;
-            this.deposit = deposit;
-            this.withdrawl = withdrawl;
-            this.balance = balance;
-        }
-
-        public LocalDate getDate() {
-            return date;
-        }
-
-        public void setDate(LocalDate date) {
-            this.date = date;
-        }
-
-        public String getDescription() {
-            return description;
-        }
-
-        public void setDescription(String description) {
-            this.description = description;
-        }
-
-        public float getDeposit() {
-            return deposit;
-        }
-
-        public void setDeposit(float deposit) {
-            this.deposit = deposit;
-        }
-
-        public float getWithdrawl() {
-            return withdrawl;
-        }
-
-        public void setWithdrawl(float withdrawl) {
-            this.withdrawl = withdrawl;
-        }
-
-        public float getBalance() {
-            return balance;
-        }
-
-        public void setBalance(float balance) {
-            this.balance = balance;
-        }
-
-        @Override
-        public String toString() {
-            return "Transaction{" +
-                    "date=" + date +
-                    ", description='" + description + '\'' +
-                    ", deposit=" + deposit +
-                    ", withdrawl=" + withdrawl +
-                    ", balance=" + balance +
-                    '}';
-        }
-
-        public String getMonthYear() {
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM-yyyy");
-            return this.date.format(formatter);
-        }
-
-        public float sumDepositWithdrawl() {
-            return this.deposit - this.withdrawl;
-        }
-    }
 
 
 //    public static class IntSumReducer
