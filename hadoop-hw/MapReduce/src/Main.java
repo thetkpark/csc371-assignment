fe337e8fda29c8fdf593a3b77744e40f4eddf7f5
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2021-11-20 13:53:14 +0700

    add only map

--- hadoop-hw/MapReduce/src/Main.java
+++ hadoop-hw/MapReduce/src/Main.java
@@ -15,37 +15,41 @@ public class Main {
     public static class TokenizerMapper
             extends Mapper<Object, Text, Text, Text>{
 
-        private String uuid = UUID.randomUUID().toString();
-        private boolean pair = true;
+//        private String uuid = UUID.randomUUID().toString();
+        private Transaction tx = null;
 
         public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-//            String[] cols = value.toString().split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)");
-//            Transaction tx = new Transaction(cols);
+            String[] tokens = value.toString().split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)");
+            if (tx != null) {
+                Transaction currentTx = new Transaction(tokens);
+                float calculatedBalance = tx.getBalance() + currentTx.sumDepositWithdrawl();
 
-            context.write(new Text(uuid), value);
-            uuid = UUID.randomUUID().toString();
-            context.write(new Text(uuid), value);
+                if (Math.abs(calculatedBalance - currentTx.getBalance()) > 0.001) {
+                    context.write(new Text(), new Text("Actual balance: " + calculatedBalance + ", Balance: " + currentTx.getBalance()));
+                }
+            }
+            tx = new Transaction(tokens);
         }
     }
 
-    public static class SumSamePairReducer extends Reducer<Text, Text, Text, Text> {
-        public void reduce(Text key, Iterable<Text> textTxs, Context context) throws IOException, InterruptedException {
-
-            Transaction[] txs = new Transaction[2];
-            int i = 0;
-
-            for (Text text : textTxs) {
-                String[] token = text.toString().split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)");
-                txs[i] = new Transaction(token);
-                i++;
-            }
-
-            float calculatedBalance = txs[0].getBalance() + txs[1].sumDepositWithdrawl();
-            if (Math.abs(calculatedBalance - txs[1].getBalance()) > 0.001) {
-                context.write(key, new Text(txs[1].toString()));
-            }
-        }
-    }
+//    public static class SumSamePairReducer extends Reducer<Text, Text, Text, Text> {
+//        public void reduce(Text key, Iterable<Text> textTxs, Context context) throws IOException, InterruptedException {
+//
+//            Transaction[] txs = new Transaction[2];
+//            int i = 0;
+//
+//            for (Text text : textTxs) {
+//                String[] token = text.toString().split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)");
+//                txs[i] = new Transaction(token);
+//                i++;
+//            }
+//
+//            float calculatedBalance = txs[0].getBalance() + txs[1].sumDepositWithdrawl();
+//            if (Math.abs(calculatedBalance - txs[1].getBalance()) > 0.001) {
+//                context.write(key, new Text(txs[1].toString()));
+//            }
+//        }
+//    }
 
 
 
@@ -54,9 +58,9 @@ public class Main {
         Job job = Job.getInstance(conf, "find error balance");
         job.setJarByClass(Main.class);
         job.setMapperClass(TokenizerMapper.class);
-        job.setNumReduceTasks(1);
-        job.setCombinerClass(SumSamePairReducer.class);
-        job.setReducerClass(SumSamePairReducer.class);
+        job.setNumReduceTasks(0);
+//        job.setCombinerClass(SumSamePairReducer.class);
+//        job.setReducerClass(SumSamePairReducer.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(Text.class);
         FileInputFormat.addInputPath(job, new Path(args[0]));
